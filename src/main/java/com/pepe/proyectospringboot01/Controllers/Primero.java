bc0308f1e0d9731bf7d7f648effa8e2ba98c973c
rusok <rusok.software@gmail.com>
2026-03-03 19:07:41 -0400

    Perfiles

--- src/main/java/com/pepe/proyectospringboot01/Controllers/Primero.java
+++ src/main/java/com/pepe/proyectospringboot01/Controllers/Primero.java
@@ -1,19 +1,27 @@
 package com.pepe.proyectospringboot01.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Primero {
+	@Value("${valor.secreto}")
+	private String val;
 	@RequestMapping("/saludo")
 	@ResponseBody
 	public String saludoAtodos() {
 		return "¡Hola a todos!";
 	}
-	@RequestMapping("/despedida") //http://localhost:8080/despedida
+	@RequestMapping("/despedida") //http://localhost:7001/despedida
 	@ResponseBody
 	public String despedida() {
 		return "¡Adiós a todos!";
+	}
+	@RequestMapping("/")
+	@ResponseBody
+	public String rescatandoValores() {
+		return "El valor secreto es: "+val;
 	}
 }
