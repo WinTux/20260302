18849e80566b4b3c203953d00755e94fa33f5bb4
rusok <rusok.software@gmail.com>
2026-03-05 20:06:07 -0400

    Metodos de petición

--- src/main/java/com/pepe/proyectospringboot01/Controllers/Pruebas.java
+++ src/main/java/com/pepe/proyectospringboot01/Controllers/Pruebas.java
@@ -6,8 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pepe.proyectospringboot01.Models.Persona;
 
 @Controller
 public class Pruebas {
@@ -22,5 +28,15 @@ public class Pruebas {
 		System.out.println("CERRANDO APLICACIÓN");
 		logger.info("El servicio se apagará.");
 		SpringApplication.exit(contexto);
+	}
+	@PostMapping("/persona") // http://localhost:7001/persona [POST]
+	@ResponseBody
+	public String pruebaMetodo(@RequestBody Persona p) {
+		System.out.printf("Nombre completo: %s %s de edad %d", p.nombre,p.apellido,p.edad);
+		return "HECHO";
+	}
+	@GetMapping("/algo/{cosita}") // http://localhost:7001/algo/{parametro}
+	public void pruebaParam(@PathVariable("cosita") String x) {
+		System.out.println(x);
 	}
 }
---
+++ src/main/java/com/pepe/proyectospringboot01/Models/Persona.java
@@ -0,0 +1,7 @@
+package com.pepe.proyectospringboot01.Models;
+
+public class Persona {
+	public String nombre;
+	public String apellido;
+	public int edad;
+}
