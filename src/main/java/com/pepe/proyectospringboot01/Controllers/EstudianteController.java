cecdb6074a2b50e918f67bd9d6a5bdf131646e2b
rusok <rusok.software@gmail.com>
2026-03-09 19:44:42 -0400

    PATCH 1

--- src/main/java/com/pepe/proyectospringboot01/Controllers/EstudianteController.java
+++ src/main/java/com/pepe/proyectospringboot01/Controllers/EstudianteController.java
@@ -1,5 +1,6 @@
 package com.pepe.proyectospringboot01.Controllers;
 
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,16 +8,22 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pepe.proyectospringboot01.Models.Estudiante;
 
+
 @Controller
+@RequestMapping("/api/v1")
 public class EstudianteController {
 	private static Map<String, Estudiante> estudiantes = new HashMap<>();
 	static {
@@ -52,4 +59,24 @@ public class EstudianteController {
 		estudiantes.put(id, est);
 		return new ResponseEntity<>("Se actualizó el estudiante "+id, HttpStatus.NO_CONTENT);
 	}
+	@DeleteMapping("/estudiante/{id}")
+	public ResponseEntity<Object> elminarEstudiante(@PathVariable("id") String id){
+		estudiantes.remove(id);
+		return new ResponseEntity<>("Se eleminó el estudiante "+id, HttpStatus.NO_CONTENT);
+	}
+	@PatchMapping("/estudiante/{id}")
+	public ResponseEntity<String> editarConPatch(@PathVariable("id") String id, @RequestBody Map<String,Object> atributosModificados){
+		Estudiante estOriginal = estudiantes.get(id);
+		atributosModificados.forEach((atributo,valorNuevo)->{
+			Field campo = ReflectionUtils.findField(Estudiante.class, atributo);
+			if(campo!=null) {
+				campo.setAccessible(true);
+				ReflectionUtils.setField(campo, estOriginal, valorNuevo);
+			}
+		});
+		estudiantes.remove(id);
+		estudiantes.put(id, estOriginal);
+		return new ResponseEntity<>("Se modificó al estudiante "+ id, HttpStatus.OK);
+	}
+	
 }
